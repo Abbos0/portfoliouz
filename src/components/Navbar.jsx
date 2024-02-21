b755d915147c9d09f610dd70da38417836bc64b9
Abbos <norqulovabbos1701@gmail.com>
2024-02-22 01:47:41 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='containerr fixed w-full h-[80px] backdrop-blur flex justify-between items-center'>
+      <div className='text-[30px] font-bold'>
+        Abbos
+      </div>
+      <div >
+          <ul className='flex gap-5 text-[#6b7688] bg-[red] font-bold  uppercase'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">My works</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div className='flex gap-5'>
+        <div>RU</div>
+        <div>ENG</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

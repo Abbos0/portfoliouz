43eeb6f4b351c99b3f80c016f8e960f6b244afa8
Abbos <norqulovabbos1701@gmail.com>
2024-02-22 17:39:18 +0500

    the best

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='containerr fixed w-full h-[80px] backdrop-blur flex justify-between items-center'>
+    <div className='containerr  fixed w-full h-[80px] backdrop-blur text-[60px] flex justify-between items-center'>
       <div className='text-[30px] font-bold'>
         Abbos
       </div>
       <div >
-          <ul className='flex gap-5 text-[#6b7688] bg-[red] font-bold  uppercase'>
+          <ul className='flex gap-5 text-[#6b7688] bg-[orange] font-bold  uppercase'>
             <li><a href="#">Home</a></li>
             <li><a href="#">Skills</a></li>
             <li><a href="#">My works</a></li>
@@ -15,7 +15,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='flex gap-5'>
-        <div>RU</div>
+        <div>RU</div>   
         <div>ENG</div>
       </div>
     </div>

28ff642ba9cb063b6257a3378032d36deafa8c9e
Abbos <norqulovabbos1701@gmail.com>
2024-02-23 18:29:21 +0500

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#EDF2F8]  w-full h-[100vh] flex items-center'>
+        <div className='flex flex-col gap-6 md:gap-10 items-center md:items-end w-full max-w-[400px]'>
+            <div className='flex justify-center items-center bg-[white] gap-10 w-full max-w-[350px] p-3 rounded-[10px] shadow-lg'>
+                <p className='text-[40px]'>👋</p>
+                <p className='flex flex-col ' >
+                    <span className='text-slate-400 text-[20px]'>Hello, I am</span>
+                    <span className='font-bold text-[30px]'>Abbos</span>
+                </p>
+            </div>
+            <div className=' bg-[white]w-full max-w-[300px] p-3  text-slate-400 text-[20px] rounded-[10px] shadow-lg'>FRONTEND/REACT DEVELOPER</div>
+            
+            <button type="button" className='text-white bg-[#313bac] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Downlaod CV</button>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { HiBars2 } from "react-icons/hi2";
 const Navbar = () => {
+
+
+  const [toggle,setToggle] = useState(false)
+  
+  console.log(toggle);
   return (
-    <div className='containerr  fixed w-full h-[80px] backdrop-blur text-[60px] flex justify-between items-center'>
+
+    <nav className='containerr  fixed w-full h-[80px] backdrop-blur text-[20px] flex justify-between items-center'>
+      
       <div className='text-[30px] font-bold'>
         Abbos
       </div>
-      <div >
-          <ul className='flex gap-5 text-[#6b7688] bg-[orange] font-bold  uppercase'>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">My works</a></li>
-            <li><a href="#">Contact</a></li>
+      
+          <ul className={`flex md:flex-row flex-col  gap-6 text-[#6b7688] font-bold  uppercase md:bg-[transparent] md:static bg-[white] md:w-auto  w-80% md:h-auto h-[100vh] ${toggle ? 'right-0' : 'right-[-100%]'} top-[80px] absolute transition-[0.8s] md:p-0 p-10`}>
+            <li><a href="#"className='links' >Home</a></li>
+            <li><a href="#" className='links' >Skills</a></li>
+            <li><a href="#" className='links' >My works</a></li>
+            <li><a href="#" className='links' >Contact</a></li>
         </ul>
-      </div>
+     
+      
       <div className='flex gap-5'>
         <div>RU</div>   
         <div>ENG</div>
       </div>
-    </div>
+      <div className=' p-1 text-[25px] bg-[#313bac] rounded-full text-[white] md:hidden block 'onClick={()=>setToggle(!toggle)}>
+          <HiBars2 />
+      </div>
+    </nav>
   )
 }
 

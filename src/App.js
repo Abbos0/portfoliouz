c0a82e5a767dae33135c38a14de270c78dd78c4e
Abbos <norqulovabbos1701@gmail.com>
2024-02-27 22:01:22 +0500

    messega

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+
+
 import './App.css';
-import Header from './components/Header';
 import Main from './pages/Main';
 
 function App() {
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -7,10 +7,10 @@ import { FaLinkedinIn } from "react-icons/fa6";
 const Social = () => {
   return (
     <div className='absolute left-[20px] bottom-[20px] md:block hidden'>
-        <a href='https://t.me/boss0_002' target='_blank' className='   text-[#6b7688]  bg-[white] w-[50px] h-[50px] flex justify-center items-center rounded-full text-[25px] transition-all hover:text-[white] hover:bg-[#313bac] mb-2'>
+        <a href='https://t.me/boss0_002' target='blank' className='   text-[#6b7688]  bg-[white] w-[50px] h-[50px] flex justify-center items-center rounded-full text-[25px] transition-all hover:text-[white] hover:bg-[#313bac] mb-2'>
             <FaTelegram />
         </a>
-        <a href='https://t.me/boss0_002' target='_blank' className=' text-[#6b7688] bg-[white] w-[50px] h-[50px] flex justify-center items-center rounded-full text-[25px] transition-all hover:text-[white] hover:bg-[#313bac]'>
+        <a href='https://t.me/boss0_002' target='blank' className=' text-[#6b7688] bg-[white] w-[50px] h-[50px] flex justify-center items-center rounded-full text-[25px] transition-all hover:text-[white] hover:bg-[#313bac]'>
             <FaLinkedinIn />
         </a>
    </div>

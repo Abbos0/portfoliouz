a2d0fea24cda372d13cc62437dec2185af2e18a5
Abbos <norqulovabbos1701@gmail.com>
2024-03-02 17:12:04 +0500

    red

---
+++ src/components/SendAfter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SendAfter = () => {
+  return (
+    <div className='text-center flex justify-center items-center absolute bottom-10 w-full max-w-[300px] md:max-w-[400px] md:text-[50px] text-[30px]   text-bold bg-[#6d9ac7] '>
+      <h1>Thank You For Getting In Touch</h1>
+    </div>
+  )
+}
+
+export default SendAfter
